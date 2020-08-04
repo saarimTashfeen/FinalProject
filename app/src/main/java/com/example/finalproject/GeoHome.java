3f306ddbde3fa6fa05fe6161018c59e783bad838
Aimen <hall0408@algonquinlive.com>
2020-08-04 18:52:52 -0400

    adding list

--- app/src/main/java/com/example/finalproject/GeoHome.java
+++ app/src/main/java/com/example/finalproject/GeoHome.java
@@ -1,26 +1,34 @@
 package com.example.finalproject;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -32,7 +40,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class GeoHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     EditText lat;
     EditText lon;
     Button btn;
@@ -40,21 +48,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     ProgressBar pb;
     city city;
     ArrayList<city> shownCities;
-
+    MyOpener mydb;
+    SQLiteDatabase db;
     Toolbar tb;
+    MyListAdapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.geohome_activity);
         lat = findViewById(R.id.latitude);
         lon = findViewById(R.id.longitude);
         btn = findViewById(R.id.button);
         lv = findViewById(R.id.listView);
         pb = findViewById(R.id.progressBar);
         tb = (Toolbar) findViewById(R.id.tb);
-        //setActionBar(tb);
         setSupportActionBar(tb);
+        shownCities = new ArrayList<>();
+        tb.setTitle("");
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
                 drawer, tb, R.string.open, R.string.close);
@@ -63,9 +73,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        lv.setAdapter(myAdapter = new MyListAdapter());
 
-        shownCities = new ArrayList<>();
-
+        mydb = new MyOpener(this);
         btn.setOnClickListener(l -> {
             if (lat.getText().toString() != "" || lat.getText().toString() != null) {
                 if (lon.getText().toString() != "" || lon.getText().toString() != null) {
@@ -76,13 +86,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     String tempLat = lat.getText().toString();
                     req.execute("https://api.geodatasource.com/cities?key=IFSY3SLSX1ZBEQHPP1KANIABBO6DSAQZ&format=xml&lat="+tempLat+""+"&lng="+tempLong+"");
                 } else {
-                    Toast.makeText(MainActivity.this, "Invalid longitude", Toast.LENGTH_SHORT);
+                    Toast.makeText(GeoHome.this, "Invalid longitude", Toast.LENGTH_SHORT);
                 }
             } else {
-                Toast.makeText(MainActivity.this, "Invalid Latitude", Toast.LENGTH_SHORT);
+                Toast.makeText(GeoHome.this, "Invalid Latitude", Toast.LENGTH_SHORT);
             }
+            myAdapter.notifyDataSetChanged();
         });
+        //loadDataFromDatabase();
 
+    }
+    private void loadDataFromDatabase() {
+        //work in progress
+        String [] columns = {mydb.COL_1, mydb.COL_2, mydb.COL_3,mydb.COL_4,mydb.COL_5,mydb.COL_6};
+        db = mydb.getWritableDatabase();
+        Cursor results = db.query(false, mydb.TABLE_NAME, columns, null, null, null, null, null, null);
+        //mydb.printCursor(results,db.getVersion());
+        int sentColumnIndex = results.getColumnIndex(mydb.COL_3);
+        int messageColIndex = results.getColumnIndex(mydb.COL_2);
+        int idColIndex = results.getColumnIndex(mydb.COL_1);
+        if (results.getCount()==0){
+            Toast.makeText(GeoHome.this,"Database is empty",Toast.LENGTH_SHORT).show();
+        }
+        while (results.moveToNext()){
+            String sent = results.getString(sentColumnIndex);
+            String text = results.getString(messageColIndex);
+            long id = results.getLong(idColIndex);
+            boolean temp;
+            if (sent.equals("1"))
+                temp = true;
+            else
+                temp = false;
+            //city = new city(id,text,temp);
+            //elements.add(message);
+        }
     }
 
     @Override
@@ -117,7 +154,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 alert.create().show();
                 break;
             case R.id.aboutAPI:
-                Toast.makeText(this,"https://www.geodatasource.com/web-service",Toast.LENGTH_SHORT);
+                Toast.makeText(this,"https://www.geodatasource.com/web-service",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.donate:
                 AlertDialog.Builder alert2 = new AlertDialog.Builder(this);
@@ -142,7 +179,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout.closeDrawer(GravityCompat.START);
         return false;
     }
+    class MyListAdapter extends BaseAdapter {
 
+        @Override
+        public int getCount() {
+            return shownCities.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return shownCities.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return (long) position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflate = getLayoutInflater();
+
+                View view = inflate.inflate(R.layout.city_list_layout, parent,false);
+                TextView text = view.findViewById(R.id.id);
+                text.setText(""+shownCities.get(position).toString());
+                TextView text2 = view.findViewById(R.id.cityName);
+                text2.setText(shownCities.get(position).cityName.toString());
+                return view;
+            }
+        }
     class GeoData  extends AsyncTask<String, Integer, String> {
 
         @Override
@@ -150,7 +215,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             //create a URL object of what server to contact:
             URL url = null;
             try {
-                String tempCountry= null, tempRegion= null, tempCity= null, tempCurrency= null, tempLatitude= null, tempLongitude = null;
+                //String tempCountry= null, tempRegion= null, tempCity= null, tempCurrency= null, tempLatitude= null, tempLongitude = null;
                 url = new URL(strings[0]);
                 HttpURLConnection urlConnection = null;
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -160,33 +225,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Log.i("check","test");
                 XmlPullParser xpp = factory.newPullParser();
                 xpp.setInput(response, "UTF-8");
-                String parameter = null;
+                city tempCity = null;
                 int eventType = xpp.getEventType(); //The parser is currently at START_DOCUMENT
                 while (eventType != XmlPullParser.END_DOCUMENT) {
-                    if (eventType == XmlPullParser.START_TAG) {
-                           if (xpp.getName().equals("country")){
-                                tempCountry = xpp.getText();
-                           }
-                            if (xpp.getName().equals("region")){
-                                tempRegion = xpp.getText();
+                    String parameter = null;
+                    switch(eventType){
+                        case XmlPullParser.START_TAG:
+                        parameter = xpp.getName();
+                        if ("response".equals(parameter)){
+                            tempCity = new city();
+                            shownCities.add(tempCity);
+                        } else if (tempCity != null){
+                            if ("country".equals(parameter)){
+                                tempCity.setCountry(xpp.nextText());
+                            } else if ("region".equals(parameter)){
+                                tempCity.setRegion(xpp.nextText());
+                            } else if ("city".equals(parameter)){
+                                tempCity.setCityName(xpp.nextText());
+                            } else if ("latitude".equals(parameter)){
+                                tempCity.setLatitude(xpp.nextText());
+                            } else if ("longitude".equals(parameter)){
+                                tempCity.setLongitude(xpp.nextText());
+                            } else if ("currency_code".equals(parameter)){
+                                tempCity.setCurrency(xpp.nextText());
                             }
-                            if (xpp.getName().equals("city")){
-                                tempCity = xpp.getText();
-                            }
-                            if (xpp.getName().equals("latitude")){
-                                tempLatitude = xpp.getText();
-                            }
-                            if (xpp.getName().equals("longitude")){
-                                tempLongitude = xpp.getText();
-                            }
-                            if (xpp.getName().equals("currency_code")){
-                                tempCurrency = xpp.getText();
-                            }
-                            city = new city(tempCountry,tempRegion,tempCity,tempCurrency,tempLatitude,tempLongitude);
-                            shownCities.add(city);
-                            Log.i("makingCity",city.toString());
+                        }
+                        break;
                     }
                     eventType = xpp.next();
+                }
+                Log.i("size",""+shownCities.size());
+                for (int i=0;i<shownCities.size();i++){
+                    Log.i("city number "+i,shownCities.get(i).toString());
                 }
 
             } catch (Exception e) {
@@ -222,6 +292,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             this.cityName = cityName;
             this.currency = currency;
             this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public void setRegion(String region) {
+            this.region = region;
+        }
+
+        public void setCityName(String cityName) {
+            this.cityName = cityName;
+        }
+
+        public void setCurrency(String currency) {
+            this.currency = currency;
+        }
+
+        public void setLatitude(String latitude) {
+            this.latitude = latitude;
+        }
+
+        public void setLongitude(String longitude) {
             this.longitude = longitude;
         }
 
@@ -262,5 +356,50 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 }
+class MyOpener extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "cities.db";
+    public static final String TABLE_NAME = "cities_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "country";
+    public static final String COL_3 = "cityName";
+    public static final String COL_4 = "currency";
+    public static final String COL_5 = "latitude";
+    public static final String COL_6 = "longitude";
+    public MyOpener(Context ct){
+        super(ct,DATABASE_NAME,null,1);
+    }
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+ TABLE_NAME +
+                "( _id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COL_1 + " integer," +
+                " "+ COL_2 +" text," +
+                " "+ COL_3 + " text," +
+                " "+ COL_4 +"text," +
+                " "+ COL_5 +"text," +
+                " "+ COL_6 +"text);"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        onCreate(db);
+    }
+    public boolean add(String country,String cityName,String currency,String latitude,String longitude){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        long result = db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_2,country);
+        cv.put(COL_3,cityName);
+        cv.put(COL_4,currency);
+        cv.put(COL_5,latitude);
+        cv.put(COL_6,longitude);
+
+        if (result ==-1)
+            return false;
+return true;
+    }
+}
 
